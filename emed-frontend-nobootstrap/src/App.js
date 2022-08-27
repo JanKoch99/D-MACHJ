70108aff014331753f8f80c660b4bc47bd328a12
Christopher Frick <christopher.frick@outlook.fr>
2022-08-27 17:47:56 +0200

    404 && qrpage

--- emed-frontend-nobootstrap/src/App.js
+++ emed-frontend-nobootstrap/src/App.js
@@ -1,8 +1,10 @@
 
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link,  } from "react-router-dom";
 import Login from './Login';
 import Home from './Home';
 import Qrcode from "./Qrcode";
+import QrcodeScanner from "./QrcodeScanner";
+
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="qrcode" element={<Qrcode/>}/>
+        <Route path="qrcode-scan" element={<QrcodeScanner/>}/>
+        
       </Routes>
     </div>
   );
