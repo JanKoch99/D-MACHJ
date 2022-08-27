993c0aec2f6ae2c2d070c6b543e0ecf407d3ee3e
Christopher Frick <christopher.frick@outlook.fr>
2022-08-27 16:23:49 +0200

    qrcode generator

--- emed-frontend-nobootstrap/src/App.js
+++ emed-frontend-nobootstrap/src/App.js
@@ -2,6 +2,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Login from './Login';
 import Home from './Home';
+import Qrcode from "./Qrcode";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="qrcode" element={<Qrcode/>}/>
       </Routes>
     </div>
   );
