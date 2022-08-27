96d4bba205d58b6a98ab904a6122f60ce1256100
Danilo-A <artztutorix@gmail.com>
2022-08-27 20:03:10 +0200

    add form fields

--- emed-frontend-nobootstrap/src/RezeptForm.js
+++ emed-frontend-nobootstrap/src/RezeptForm.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import SimpleInput from './components/simpleInput'
 
 function RezeptForm() {
   return (
-    <div>RezeptForm</div>
+    <div className="content py-32">
+      <h1>Willkommen zu der Praxis</h1>
+      <h2 className="mt-4">Verordnung hinzufügen</h2>
+      <div className="mt-10">
+        <h3>Zustellender Arzt</h3>
+        <div className="grid grid-cols-2 gap-6 mt-4">
+          <SimpleInput
+            label="Name"
+            name="name"
+            type="text"
+            value="Danilo Abreu Ott"
+          />
+          <SimpleInput 
+            label="ZSR-Nr"
+            name="zsr"
+            type="text"
+            placeholder="Danilo Abreu Ott"
+          />
+          <SimpleInput 
+            label="Praxis"
+            name="praxis"
+            type="text"
+            placeholder="Danilo Abreu Ott"
+          />
+          <SimpleInput 
+            label="Praxisadresse"
+            name="name"
+            type="text"
+            placeholder="Danilo Abreu Ott"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- emed-frontend-nobootstrap/src/components/simpleInput.js
+++ emed-frontend-nobootstrap/src/components/simpleInput.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function simpleInput({name, type, label, placeholder}) {
+function simpleInput({name, type, label, placeholder, value}) {
   return (
     <div>
       <label htmlFor={name} className="block text-sm font-bold text-gray-800 mb-2">
@@ -13,6 +13,7 @@ function simpleInput({name, type, label, placeholder}) {
           type={type}
           placeholder={placeholder}
           required
+          value={value}
           className="appearance-none block w-full p-4 border border-gray-300 rounded placeholder-gray-400 focus:outline-none focus:ring-primary focus:border-primary"
         />
       </div>
