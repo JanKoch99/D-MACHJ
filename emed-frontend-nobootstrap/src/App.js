2dd1e66034ee13a7e541250ce8e3d2ff04cbe24d
Danilo-A <artztutorix@gmail.com>
2022-08-27 16:39:21 +0200

    :lipstick: ci implement

--- emed-frontend-nobootstrap/src/App.js
+++ emed-frontend-nobootstrap/src/App.js
@@ -2,13 +2,23 @@
 import { Routes, Route, Link } from "react-router-dom";
 import Login from './Login';
 import Home from './Home';
+import { IconUser } from '@tabler/icons';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div >
-      <Link to="login">
-        Login
-      </Link>
+    <div>
+      <div className="flex items-center">
+        <Link to="login">
+          <IconUser />
+        </Link>
+        <Link to="/">
+          <img src={logo} alt="Logo EMed" />
+        </Link>
+        <Link to="login">
+          <IconUser />
+        </Link>
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
