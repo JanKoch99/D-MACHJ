16cf322d153ae09ac15c6a10b7b2a1e8d6532ef0
4l3s5i0Z <alessio.petrone@students.unibe.ch>
2022-08-28 11:52:17 +0200

    Update finish.js

--- emed-frontend-nobootstrap/src/pharmacy/finish.js
+++ emed-frontend-nobootstrap/src/pharmacy/finish.js
@@ -1,10 +1,16 @@
 import { IconArrowNarrowRight } from '@tabler/icons';
-import React from 'react'
+import React, {useCallback} from 'react'
 import EmedButton from '../components/emedButton';
 import Layout from '../components/layout/layout';
 import IllustrationFinish from '../illustrations/finish.svg'
+import {useNavigate} from "react-router-dom";
 
 function Finish() {
+
+  const navigate = useNavigate();
+
+  const handleOnClickZurück = useCallback(() => navigate('/', {replace: true}), [navigate]);
+
   return (
     <Layout>
         <div className="mt-32 flex flex-col justify-center items-center">
@@ -12,11 +18,13 @@ function Finish() {
           <div className="text-center mt-14">
             <h1>Fertig!</h1>
             <p className="font-headline text-gray-600 text-lg mt-2 mb-6">Rezept erfolgreich ausgestellt</p>
+            <div onClick={handleOnClickZurück}>
             <EmedButton
               icon={<IconArrowNarrowRight/>}
             >        
               Zur Startseite
             </EmedButton>
+          </div>
           </div>
         </div>
     </Layout>
